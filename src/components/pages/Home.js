1279bf3fd019e7243789bd4901181322ead8ec59
Cmac2552 <colinmac1004@gmail.com>
2022-12-22 13:19:32 -0500

    stuff

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,12 +6,7 @@ import "./Home.css"
 export default function Home() {
     return (
         <>
-            <br></br>
-            <p className = "words">My name is Colin MacMaster and I am currently a rising senior at Virginia Tech and
-            Software Engineering Intern at BTI360. Clicking on the tabs at the top will take you to my projects,
-            experience, and how to contact me.</p>
-            <br></br>
-            <img class = "face" src ={profile_pic} alt = "My Face"/>
+            
         </>
     );
 }
